0146c69f61f4e310f35f81b27d6e51e0b254883c
Joe Ryan <joe@clanryan.us>
2015-03-05 21:09:04 -0500

    create numgame_test.py, no initial tests

---
+++ numbers/numgame_test.py
@@ -0,0 +1,12 @@
+# tests for number guessing game numgame.py
+import unittest
+import numgame
+
+class NumgameTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_new_game(self):
+        pass
+
+    
