e28d5433b3cbfdf8959148500e3ab1eee962c042
Joe Ryan <joe@clanryan.us>
2015-03-11 22:31:26 -0400

    refactor numgame to class NumGame, setup first test

--- numbers/numgame.py
+++ numbers/numgame.py
@@ -3,35 +3,31 @@
 # all output for the game will be printed in the console
 import random
 
-target = 0
-max_range = 100
+class NumGame:
+  def  __init__(self):
+    self.max_range = 100
+    self.target =  random.randint(0, self.max_range)
+#    self.new_game()
 
-# helper function to start and restart the game
-def new_game():
-    global target
-    target = random.randint(0, max_range)
+  # helper function to start and restart the game
+  def new_game(self):
+    self.target = random.randint(0, self.max_range)
 
-
-# define event handlers for control panel
-def range100():
-    global max_range 
-    max_range = 100
-    new_game()
+  # define event handlers for control panel
+  def range100(self):
+    self.max_range = 100
+    self.new_game()
     
 
-def range1000():
-    global max_range
-    max_range = 1000
-    new_game()
+  def range1000(self):
+    self.max_range = 1000
+    self.new_game()
     
-def input_guess(guess):
+  def input_guess(self, guess):
+    print guess
     # main game logic goes here	
     
-    # remove this when you add your code
-    pass
-
-    
-# create frame
+# create frame for graphical game play
 
 
 # register event handlers for control elements and start frame
@@ -39,10 +35,10 @@ def input_guess(guess):
 
 # call new_game 
 if __name__ == "__main__":
-    new_game()
-    print(target)
-    range1000()
-    print(target)
-    range100()
-    print(target)
+    game = NumGame()
+    print(game.target)
+    game.range1000()
+    print(game.target)
+    game.range100()
+    print(game.target)
 # always remember to check your completed program against the grading rubric
--- numbers/numgame_test.py
+++ numbers/numgame_test.py
@@ -4,10 +4,13 @@ import numgame
 
 class NumgameTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        pass 
 
-    def test_new_game(self):
-        self.assertTrue(True)
+    def test_new_game_100(self):
+        #  game.new_game()
+        game = numgame.NumGame()
+        self.assertTrue(game.target >= 0)
+        self.assertTrue(game.target <= 100)
 
 if __name__ == "__main__":
     unittest.main()
