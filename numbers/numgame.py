6fd6d9088798dfc4380d796de6afd60a21456e36
Joe Ryan <joe@clanryan.us>
2015-03-13 22:25:25 -0400

    final code for codeskulptor submission

--- numbers/numgame.py
+++ numbers/numgame.py
@@ -3,6 +3,7 @@
 # all output for the game will be printed in the console
 import random
 
+<<<<<<< HEAD
 secret_number = 0
 max_range = 100
 guess_message = "Number range 0 - 100"
@@ -28,21 +29,39 @@ def range100():
     global max_range 
     max_range = 100
     new_game()
+=======
+class NumGame:
+  def  __init__(self):
+    self.max_range = 100
+    self.target =  random.randint(0, self.max_range)
+#    self.new_game()
+
+  # helper function to start and restart the game
+  def new_game(self):
+    self.target = random.randint(0, self.max_range)
+
+  # define event handlers for control panel
+  def range100(self):
+    self.max_range = 100
+    self.new_game()
+>>>>>>> a35fdb67b5fc861aeb580887e9021774ca45d280
     
 
-def range1000():
-    global max_range
-    max_range = 1000
-    new_game()
+  def range1000(self):
+    self.max_range = 1000
+    self.new_game()
     
-def input_guess(guess):
+  def check_guess(self, guess):
+    if (guess < self.target): 
+      return -1
+    elif (guess > self.target):
+      return 1
+    else:
+      return 0
+
     # main game logic goes here	
     
-    # remove this when you add your code
-    pass
-
-    
-# create frame
+# create frame for graphical game play
 
 
 # register event handlers for control elements and start frame
@@ -50,7 +69,17 @@ def input_guess(guess):
 
 # call new_game 
 if __name__ == "__main__":
+<<<<<<< HEAD
 
 # http://www.codeskulptor.org/#user39_xebz8UsTuy_7.py
 
 
+=======
+    game = NumGame()
+    print(game.target)
+    game.range1000()
+    print(game.target)
+    game.range100()
+    print(game.target)
+# always remember to check your completed program against the grading rubric
+>>>>>>> a35fdb67b5fc861aeb580887e9021774ca45d280
--- numbers/numgame_test.py
+++ numbers/numgame_test.py
@@ -4,6 +4,7 @@ from numgame import *
 
 class NumgameTestCase(unittest.TestCase):
     def setUp(self):
+<<<<<<< HEAD
        new_game()
 
     def test_new_game(self):
@@ -13,7 +14,37 @@ class NumgameTestCase(unittest.TestCase):
         range1000()
         self.assertTrue(max_range == 1000)
         
+=======
+        pass 
 
+    def test_new_game_100(self):
+        #  game.new_game()
+        game = numgame.NumGame()
+        self.assertTrue(game.target >= 0)
+        self.assertTrue(game.target <= 100)
+>>>>>>> a35fdb67b5fc861aeb580887e9021774ca45d280
+
+    def test_range1000(self):
+        game = numgame.NumGame()
+        game.range1000()
+        self.assertTrue(game.target >= 0)
+        self.assertTrue(game.target <= 1000)
+
+    def test_check_guess_actual(self):
+        game = numgame.NumGame()
+        game.target = 55
+        self.assertTrue(game.check_guess(55) == 0)
+ 
+    def test_check_guess_lower(self):
+        game = numgame.NumGame()
+        game.target = 55
+        self.assertTrue(game.check_guess(54) == -1)
+ 
+    def test_check_guess_higher(self):
+        game = numgame.NumGame()
+        game.target = 55
+        self.assertTrue(game.check_guess(56) == 1)
+ 
 if __name__ == "__main__":
     unittest.main()
 
