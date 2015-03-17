f7567479ac49f01a8e3c10b265ba064f6ec97fda
Joe Ryan <joe@clanryan.us>
2015-03-16 21:03:33 -0400

    create game board for number guessing game

--- numbers/numgame-tk.py
+++ numbers/numgame-tk.py
@@ -1,6 +1,23 @@
-import Tkinter as Tk
+from Tkinter import Tk, Frame, BOTH
 import numgame
 
-game = numgame.NumGame()
+class GameBoard(Frame):
+    game = numgame.NumGame()
 
-print game.target
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background='white')
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Number Guessing Game")
+        self.pack(fill=BOTH, expand=1)
+
+def main():
+        root = Tk()
+        root.geometry("350x250+300+300")
+        app = GameBoard(root)
+        root.mainloop()
+
+if __name__ =='__main__':
+    main()
