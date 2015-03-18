c8f784166d1b1b8126da477431aa8d8600fd6423
Joe Ryan <joe@clanryan.us>
2015-03-17 21:28:00 -0400

    create pong play area

--- pong/pong.py
+++ pong/pong.py
@@ -6,12 +6,16 @@ class PongGame(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text = 'Quit',
                                     command=self.quit)
+        self.playArea = tk.Canvas(bg='white', height=850, width = 1150)
+        self.playArea.grid(padx=25, pady=15)
         self.quitButton.grid()
 
 pong = PongGame()
+pong.master.geometry("1200x900+300+300")
 pong.master.title('Game of Pong')
 pong.mainloop()
