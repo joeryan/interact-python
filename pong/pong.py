fad7660748ba146d53a69ffbab5e016805ec7ce0
Joe Ryan <joe@clanryan.us>
2015-03-20 19:10:51 -0400

    change playing surface, final codeskulptor.org url

--- pong/pong.py
+++ pong/pong.py
@@ -2,27 +2,30 @@
 import Tkinter as tk
 
 class PongGame(tk.Frame):
-    FIELD_HEIGHT = 850
+    FIELD_HEIGHT = 650
     FIELD_WIDTH = 1150
+    PAD_HEIGHT = 80
+    PAD_WIDTH = 20
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
-        
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text = 'Quit',
                                     command=self.quit)
-        self.playArea = tk.Canvas(bg='white', height=self.FIELD_HEIGHT, 
+        self.playArea = tk.Canvas(bg='black', height=self.FIELD_HEIGHT, 
                                   width = self.FIELD_WIDTH)
         self.playArea.grid(padx=25, pady=15)
         self.quitButton.grid()
 
-pong = PongGame()
-pong.master.geometry("1200x900+300+300")
-pong.master.title('Game of Pong')
-pong.mainloop()
+def main():
+    pong = PongGame()
+    pong.master.geometry("1240x820+300+300")
+    pong.master.title('Game of Pong')
+    pong.mainloop()
 
-
-#online submission url - http://www.codeskulptor.org/#user39_wJkf7WYddA_7.py
+if __name__ == '__main__':
+    main()
+#online submission url - http://www.codeskulptor.org/#user39_wJkf7WYddA_17.py
