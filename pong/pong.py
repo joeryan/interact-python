45cbe3fbf6637a9c6e3548257d51ad6e2c44b3ee
Joe Ryan <joe@clanryan.us>
2015-03-19 22:19:32 -0400

    update codeskulptor.org url for online submission

--- pong/pong.py
+++ pong/pong.py
@@ -2,6 +2,9 @@
 import Tkinter as tk
 
 class PongGame(tk.Frame):
+    FIELD_HEIGHT = 850
+    FIELD_WIDTH = 1150
+
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
@@ -11,7 +14,8 @@ class PongGame(tk.Frame):
     def createWidgets(self):
         self.quitButton = tk.Button(self, text = 'Quit',
                                     command=self.quit)
-        self.playArea = tk.Canvas(bg='white', height=850, width = 1150)
+        self.playArea = tk.Canvas(bg='white', height=self.FIELD_HEIGHT, 
+                                  width = self.FIELD_WIDTH)
         self.playArea.grid(padx=25, pady=15)
         self.quitButton.grid()
 
@@ -19,3 +23,6 @@ pong = PongGame()
 pong.master.geometry("1200x900+300+300")
 pong.master.title('Game of Pong')
 pong.mainloop()
+
+
+#online submission url - http://www.codeskulptor.org/#user39_wJkf7WYddA_7.py
