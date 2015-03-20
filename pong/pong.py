93666d40178826102338c6e3d4450e76b836f809
Joe Ryan <joe@clanryan.us>
2015-03-20 19:37:16 -0400

    draw gutters and center line

--- pong/pong.py
+++ pong/pong.py
@@ -2,10 +2,10 @@
 import Tkinter as tk
 
 class PongGame(tk.Frame):
-    FIELD_HEIGHT = 650
-    FIELD_WIDTH = 1150
-    PAD_HEIGHT = 80
-    PAD_WIDTH = 20
+    FLD_H = 650
+    FLD_W = 1150
+    PAD_H = 80
+    PAD_W = 20
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -15,10 +15,18 @@ class PongGame(tk.Frame):
     def createWidgets(self):
         self.quitButton = tk.Button(self, text = 'Quit',
                                     command=self.quit)
-        self.playArea = tk.Canvas(bg='black', height=self.FIELD_HEIGHT, 
-                                  width = self.FIELD_WIDTH)
+        self.playArea = tk.Canvas(bg='black', height=self.FLD_H, 
+                                  width = self.FLD_W)
         self.playArea.grid(padx=25, pady=15)
         self.quitButton.grid()
+        left_gut = self.playArea.create_line(self.PAD_W, 0, self.PAD_W, self.FLD_H, 
+                                             width = 2, fill  = 'white')
+        right_gut = self.playArea.create_line((self.FLD_W - self.PAD_W), 0,
+                                              (self.FLD_W - self.PAD_W), 
+                                             self.FLD_H, width = 2, fill  = 'white')
+        center_line = self.playArea.create_line((self.FLD_W/2), 0,
+                                              (self.FLD_W/2), self.FLD_H, 
+                                              width = 2, fill  = 'white')
 
 def main():
     pong = PongGame()
